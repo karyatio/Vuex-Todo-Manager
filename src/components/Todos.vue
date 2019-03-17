6f4fb0a3a05d50a0d7695b18162c6ac18637a335
unknown <karya.tiosaputra@gmail.com>
2019-03-17 18:32:41 +0700

    Todos, Todo Component & Vuex Setup

---
+++ src/components/Todos.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h3>Hallo</h3>
+    <div class="todos">
+      <div v-for="todo in allTodos" v-bind:key="todo.id" class="todo">{{todo.title}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "Todos",
+  methods: {
+    ...mapActions(["fetchTodos"])
+  },
+  computed: mapGetters(["allTodos"]),
+  created() {
+    this.fetchTodos();
+  }
+};
+</script>
+
+<style scoped>
+.todos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+}
+
+.todo {
+  border: 1px solid #ccc;
+  background: #41b883;
+  padding: 1rem;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
+</style>
